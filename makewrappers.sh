d52db2f99a5268503997d95a970abc897b11aa47
Alexey Borzenkov <snaury@gmail.com>
2008-06-11 14:42:03 +0400

    Changed target directory to /bin-public

--- makewrappers.sh
+++ makewrappers.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-./makewrapper.sh $HOME/shell-runners/cg.exe /bin/cg myvars.h
-./makewrapper.sh $HOME/shell-runners/git.exe ${DFGITROOT}/bin/git myvars.h
-./makewrapper.sh $HOME/shell-runners/gitk.exe ${DFGITROOT}/bin/gitk myvars.h
-./makewrapper.sh $HOME/shell-runners/cygwin-ls.exe /bin/ls myvars.h
-./makewrapper.sh $HOME/shell-runners/cygwin-bash.exe /bin/bash myvars.h
-./makewrapper.sh $HOME/shell-runners/cygwin-chmod.exe /bin/chmod myvars.h
+./makewrapper.sh /bin-public/cg.exe /bin/cg myvars.h
+./makewrapper.sh /bin-public/git.exe ${DFGITROOT}/bin/git myvars.h
+./makewrapper.sh /bin-public/gitk.exe ${DFGITROOT}/bin/gitk myvars.h
+./makewrapper.sh /bin-public/cygwin-ls.exe /bin/ls myvars.h
+./makewrapper.sh /bin-public/cygwin-bash.exe /bin/bash myvars.h
+./makewrapper.sh /bin-public/cygwin-chmod.exe /bin/chmod myvars.h
